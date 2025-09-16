a00f2acb2a761ae3c9cd9cc900cddf2a3d9f106e
Harshitsharma1022 <harshitsharma1032006@gmail.com>
2025-09-16 20:49:25 +0530

    16/9/25

--- src/CWH_26_arrays.java
+++ src/CWH_26_arrays.java
@@ -1,2 +1,22 @@
 public class CWH_26_arrays {
+    public static void main(String[] args) {
+//        three ways to create array in java
+//        1.Declaration and memory allocation
+//        int [] marks = new int [5];
+
+//        2.Declaration and then memory allocation
+//        int [] marks;
+//        marks = new int[5];
+//       marks[0]=100;
+//       marks[1]=60;
+//       marks[2]=70;
+//       marks[3]=90;
+//       marks[4]=86;
+//       marks[5]=96; - throws an error out of assigned array
+
+//        3.Declaration memory allocation and initialization together
+        int [] marks ={98, 45, 79, 99, 80};
+
+        System.out.println(marks[1]);
+    }
 }
